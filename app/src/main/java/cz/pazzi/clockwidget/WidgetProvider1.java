7b67d87a7b75df7878af2e53cc7cf1ced71a5f5f
PavelDrabek <pavel@craneballs.com>
2015-09-24 11:06:29 +0200

    layout progress

--- app/src/main/java/cz/pazzi/clockwidget/WidgetProvider1.java
+++ app/src/main/java/cz/pazzi/clockwidget/WidgetProvider1.java
@@ -4,6 +4,8 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.GridView;
+import android.widget.RemoteViews;
 import android.widget.Toast;
 
 /**
@@ -11,11 +13,15 @@ import android.widget.Toast;
  */
 public class WidgetProvider1 extends AppWidgetProvider {
 
+    private GridView gridView;
+
+    public WidgetProvider1() {
+        super();
+    }
+
     @Override
     public void onEnabled(Context context) {
         super.onEnabled(context);
-        MyClock custom_clock = new MyClock(context);
-        custom_clock.measure(100,20);
     }
 
     @Override
